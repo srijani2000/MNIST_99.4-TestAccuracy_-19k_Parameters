78381ffa0f9fe774ba6e4a03f0254a686b825800
Srijani Debnath <srijanidebnath2@gmail.com>
2025-09-19 10:19:11 +0530

    Code changes done on Test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import torch
-from assignment6 import Net
+from basic_dnn_mnist import Net
 
 def count_parameters(model):
     """Count and format the total number of parameters"""
